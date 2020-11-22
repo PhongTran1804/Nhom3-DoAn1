8f668c4b5c521bd3111cbd03a13641b4f8112120
DoTranMinhNhat <74851242+DoTranMinhNhat@users.noreply.github.com>
2020-11-22 15:02:11 +0700

    Update and rename SoNguyenToNhoHonHoacBangN.cpp to CacSoNguyenToNhoHonHoacBangN.cpp

---
+++ CacSoNguyenToNhoHonHoacBangN.cpp
@@ -0,0 +1,8 @@
+﻿int CacSoNguyenToNhoHonHoacBangN(int n) // Tìm các số nguyên tố nhỏ hơn hoặc bằng n
+{
+    cout << "Cac so nguyen to nho hon hoac bang n: "; // xuất ra màn hình dòng chữ trong ngoặc
+    for (int i = 1; i <= n; i++) { // cho i chạy từ 1 đến n, tăng i lên từng đơn vị
+        if (CheckPrimeNumber(i) == true) cout << i << " "; // số nào thỏa điều kiện của số nguyên tố, nhận và xuất ra màn hình
+    }
+    cout << endl; // xuống dòng
+}
--- SoNguyenToNhoHonHoacBangN.cpp
+++
@@ -1,8 +0,0 @@
-﻿int PrimeNumber(int n) // Tìm các số nguyên tố nhỏ hơn hoặc bằng n
-{
-    cout << "All prime numbers less or equal to n are: ";
-    for (int i = 1; i <= n; i++) {
-        if (CheckPrimeNumber(i) == true) cout << i << " ";
-    }
-    cout << endl;
-}
