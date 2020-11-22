173013c957ba415e0c7d5763686ca070906387d6
DoTranMinhNhat <74851242+DoTranMinhNhat@users.noreply.github.com>
2020-11-22 14:35:42 +0700

    Add files via upload

---
+++ 1b.cpp
@@ -0,0 +1,8 @@
+﻿int PrimeNumber(int n)
+{
+    cout << "All prime numbers less or equal to n are: ";
+    for (int i = 1; i <= n; i++) {
+        if (CheckPrimeNumber(i) == true) cout << i << " ";
+    }
+    cout << endl;
+}
