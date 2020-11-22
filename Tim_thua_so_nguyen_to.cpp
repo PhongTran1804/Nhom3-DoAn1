44e29740e7eac94daa780e70b317ef2c1978e868
PhongTran1804 <74850769+PhongTran1804@users.noreply.github.com>
2020-11-22 20:51:27 +0700

    Hàm phân tích N thành các thừa số nguyên tố

--- Tim_thua_so_nguyen_to.cpp
+++ Tim_thua_so_nguyen_to.cpp
@@ -1,20 +1,18 @@
-
+#include"Header.h"
 void tim_thua_so_nguyen_to(int n) {
 
 	for (int i = 2; i <= n; i++)
 	{
 		if (KTSNT(i) == true)
 		{
-			if (n % i == 0) {
+			while (n % i == 0)
+			{
 				cout << " " << i;
-				while (n % i == 0) {
-					n /= i;
-				}
+				n /= i;
 			}
 		}
 	}
 }
-
 
 
 
