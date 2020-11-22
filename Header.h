426ef2e8505dacb84ee9f0c34f0e93214f77e75a
PhongTran1804 <74850769+PhongTran1804@users.noreply.github.com>
2020-11-22 20:53:57 +0700

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,9 @@
+#include<iostream>
+#include <cmath>
+using namespace std;
+#include <math.h>
+bool KTSNT(int x);
+void SNT_nhohonhoacbang_n(int n);
+void phaiquatrai(int n);
+void tim_thua_so_nguyen_to(int n);
+
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "Header.h"
+int main()
+{
+	int n;
+	cout << "Nhap so nguyen duong n: ";
+	cin >> n;
+	if (KTSNT(n) == true) cout << n << " la so nguyen to" << endl;
+	else cout << n << " khong la so nguyen to" << endl;
+	SNT_nhohonhoacbang_n(n); cout << endl;
+	phaiquatrai(n); cout << endl;
+	cout << n << " phan tich thanh thua so nguyen to: ";
+	tim_thua_so_nguyen_to(n);
+	cout << endl;
+}
