036db30fead3a0c3784bab9634e49a6008adecd7
DoTranMinhNhat <74851242+DoTranMinhNhat@users.noreply.github.com>
2020-11-22 15:09:06 +0700

    Tìm các số nguyên tố nhỏ hơn hoặc bằng n

--- Các Số Nguyên Tố Nhỏ Hơn Hoặc Bằng n.cpp
+++ Các Số Nguyên Tố Nhỏ Hơn Hoặc Bằng n.cpp
@@ -1,8 +1,8 @@
-﻿int CacSoNguyenToNhoHonHoacBangN(int n) // Tìm các số nguyên tố nhỏ hơn hoặc bằng n
+﻿int SNT_nhohonhoacbang_n(int n) // Tìm các số nguyên tố nhỏ hơn hoặc bằng n
 {
     cout << "Cac so nguyen to nho hon hoac bang n: "; // xuất ra màn hình dòng chữ trong ngoặc
     for (int i = 1; i <= n; i++) { // cho i chạy từ 1 đến n, tăng i lên từng đơn vị
-        if (CheckPrimeNumber(i) == true) cout << i << " "; // số nào thỏa điều kiện của số nguyên tố, nhận và xuất ra màn hình
+        if (KTSNT(i) == true) cout << i << " "; // số nào thỏa điều kiện của số nguyên tố, nhận và xuất ra màn hình
     }
     cout << endl; // xuống dòng
 }
