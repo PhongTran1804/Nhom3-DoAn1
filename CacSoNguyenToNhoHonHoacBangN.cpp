312171e1c23c60a922de68cc64eba94449c9a60a
PhongTran1804 <74850769+PhongTran1804@users.noreply.github.com>
2020-11-22 15:29:17 +0700

    Ham in ra cac so nguyen to nho hon hoac bang N

--- CacSoNguyenToNhoHonHoacBangN.cpp
+++ CacSoNguyenToNhoHonHoacBangN.cpp
@@ -1,4 +1,4 @@
-﻿ SNT_nhohonhoacbang_n(int n) // Tìm các số nguyên tố nhỏ hơn hoặc bằng n
+void SNT_nhohonhoacbang_n(int n) // Tìm các số nguyên tố nhỏ hơn hoặc bằng n
 {
     cout << "Cac so nguyen to nho hon hoac bang n: "; // xuất ra màn hình dòng chữ trong ngoặc
     for (int i = 1; i <= n; i++) { // cho i chạy từ 1 đến n, tăng i lên từng đơn vị
